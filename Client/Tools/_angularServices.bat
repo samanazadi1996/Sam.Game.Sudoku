3463029b69cff905b91886263363efaf92e81622
saman azadi <samanazadi1996@gmail.com>
2025-11-21 15:41:49 +0330

    Push at 21-Fri-11_15-41

---
+++ Client/Tools/_angularServices.bat
@@ -0,0 +1,3 @@
+cd Sam.OpenApi
+
+OpenApi.exe "angular" "https://localhost:5001/swagger/v1/swagger.json" "..\..\Angular\src\app\core\services"
--- Tools/_angularServices.bat
+++
@@ -1,3 +0,0 @@
-cd Sam.OpenApi
-
-OpenApi.exe "angular" "https://localhost:5001/swagger/v1/swagger.json" "..\..\Client\Angular\src\app\core\services"
